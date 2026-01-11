c684f8fe59a84314b2e0ebdf4f84a78095bb61a8
aenansh <231030317@juitsolan.in>
2026-01-11 14:13:19 +0530

    updated node and prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,12 @@
-import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../generated/prisma/client'
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
+import dotenv from "dotenv";
 
-const connectionString = `${process.env.DATABASE_URL}`
+dotenv.config();
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+const connectionString = `${process.env.DATABASE_URL}`;
 
-export { prisma }
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
