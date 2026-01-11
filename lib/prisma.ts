20d42c53e5b9307c0ed2b66dd465f834842abd5a
aenansh <231030317@juitsolan.in>
2026-01-11 14:18:35 +0530

    minor change

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,6 @@
+import "dotenv/config"
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
