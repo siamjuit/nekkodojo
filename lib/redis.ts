70d912907d6dd0b0b3473cc862032b1a4ba13be6
aenansh <231030317@juitsolan.in>
2026-01-24 13:42:12 +0530

    fix: added environment variables to vercel

--- lib/redis.ts
+++ lib/redis.ts
@@ -4,11 +4,7 @@ const getRedisClient = () => {
   if (!process.env.UPSTASH_REDIS_REST_URL || !process.env.UPSTASH_REDIS_REST_TOKEN) {
     throw new Error("Redis credentials are missing from .env.local");
   }
-
-  return new Redis({
-    url: process.env.UPSTASH_REDIS_REST_URL,
-    token: process.env.UPSTASH_REDIS_REST_TOKEN,
-  });
+  return Redis.fromEnv();
 };
 
 export const redis = getRedisClient();
