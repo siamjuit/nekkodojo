680811b4618e7aded217ae27a3eda89348044594
aenansh <231030317@juitsolan.in>
2026-01-24 13:30:41 +0530

    bug: environment variables weren't found

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,8 +1,14 @@
 import { Redis } from "@upstash/redis";
 
-const url = process.env.UPSTASH_REDIS_REST_URL!;
-const token = process.env.UPSTASH_REDIS_REST_TOKEN!;
-export const redis = new Redis({
-  url,
-  token,
-});
+const getRedisClient = () => {
+  if (!process.env.UPSTASH_REDIS_REST_URL || !process.env.UPSTASH_REDIS_REST_TOKEN) {
+    throw new Error("Redis credentials are missing from .env.local");
+  }
+
+  return new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  });
+};
+
+export const redis = getRedisClient();
